c2a208f18ed437186806395bc2e32a54bea27db7
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2019-01-25 16:59:43 +0100

    Init RxExternalAccessory class

---
+++ RxExternalAccessory/Sources/RxExternalAccessory.swift
@@ -0,0 +1,14 @@
+//
+//  RxExternalAccessory.swift
+//  RxExternalAccessory
+//
+//  Created by Kamil Harasimowicz on 25/01/2019.
+//
+
+import ExternalAccessory
+import RxSwift
+
+final class RxExternalAccessory {
+    
+    private let supportedProtocols = Set<String>(Bundle.main.supportedAccessoryProtocols)
+}
