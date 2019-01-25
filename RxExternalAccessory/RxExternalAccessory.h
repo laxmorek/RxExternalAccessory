0b12aca4f0a524875db8d36b0ee3a76a236b7998
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2019-01-25 16:43:11 +0100

    Init project

---
+++ RxExternalAccessory/RxExternalAccessory.h
@@ -0,0 +1,18 @@
+//
+//  RxExternalAccessory.h
+//  RxExternalAccessory
+//
+//  Created by Kamil Harasimowicz on 25/01/2019.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxExternalAccessory.
+FOUNDATION_EXPORT double RxExternalAccessoryVersionNumber;
+
+//! Project version string for RxExternalAccessory.
+FOUNDATION_EXPORT const unsigned char RxExternalAccessoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxExternalAccessory/PublicHeader.h>
+
+
---
+++ RxExternalAccessoryTests/RxExternalAccessoryTests.swift
@@ -0,0 +1,15 @@
+//
+//  RxExternalAccessoryTests.swift
+//  RxExternalAccessoryTests
+//
+//  Created by Kamil Harasimowicz on 25/01/2019.
+//
+
+import XCTest
+
+@testable import RxExternalAccessory
+
+class RxExternalAccessoryTests: XCTestCase {
+
+    func test() { }
+}
